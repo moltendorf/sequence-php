890f6fa773e5d2bd4d6fa5c77e578b7aa34177be
Matthew Oltendorf <moltendorf@gmail.com>
2013-09-14 23:05:50 -0700

    Added base hook class.

---
+++ system/blink/root/hook.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace blink\root {
+	use blink as b;
+	use blink\functions as f;
+
+	class hook extends b\bind {
+
+		/*
+		 * Implementation of b\bind.
+		 */
+
+		/**
+		 *
+		 */
+		protected function construct() {
+
+		}
+
+		/*
+		 * End implementation of b\bind.
+		 */
+	}
+}
