390717adb05d98b1ecb633f5258820406696c48f
Matthew Oltendorf <moltendorf@gmail.com>
2014-04-03 17:49:39 -0700

    Changed error 500 message to be a little more concise.

--- system/language/en-US.php
+++ system/language/en-US.php
@@ -2,6 +2,7 @@
 
 return [
 	'ERROR_500'			=> 'Internal Server Error',
+	'ERROR_500_MESSAGE'	=> 'The server has encountered an error and could not process your request.',
 	'EXCEPTION'			=> 'Exception',
 	'TYPE'				=> 'Type',
 	'MESSAGE'			=> 'Message',
