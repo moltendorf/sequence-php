9a7b4f971c870967576fceea8a2fed593d4405e0
Matthew Oltendorf <moltendorf@gmail.com>
2014-07-29 11:10:17 -0700

    Added a few text functions.

--- system/functions.php
+++ system/functions.php
@@ -44,4 +44,51 @@ namespace blink\functions {
 
 		return false;
 	}
+
+	/**
+	 * @param string $text
+	 * @param int    $tab
+	 *
+	 * @return string
+	 */
+	function text_normalize($text, $tab = 4) {
+		echo $text;
+
+		// Normalize all line breaks to line feeds, then split the text into an array at each line break.
+		$text = explode("\n", str_replace(["\r\n", "\r"], "\n", $text));
+
+		// Convert tabs to spaces.
+		for ($i = 0, $j = count($text); $i < $j; ++$i) {
+			$post = $text[$i];
+			$line = '';
+
+			// Used to calculate the size of each tab without having to recount the entire length of the string.
+			$length = 0;
+
+			while (($position = mb_strpos($post, "\t")) !== false) {
+				// Split string into two, stripping the tab in the process.
+				$pre  = $position ? mb_substr($post, 0, $position) : '';
+				$post = mb_substr($post, $position + 1);
+
+				$length += mb_strlen($pre);
+				$size = $tab - ($length % $tab);
+				$length += $size;
+
+				$line .= $pre . str_repeat(' ', $size);
+			}
+
+			$text[$i] = $line . $post;
+		}
+
+		return implode("\n", $text);
+	}
+
+	/**
+	 * @param string $text
+	 *
+	 * @return string
+	 */
+	function text_format($text) {
+		return nl2br(str_replace(' ', '&nbsp;', htmlspecialchars($text, ENT_COMPAT | ENT_DISALLOWED | ENT_HTML5)));
+	}
 }
