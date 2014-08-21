cd4366276de884d07a7ad7c7cabd736f753e5ab8
Matthew Oltendorf <moltendorf@gmail.com>
2014-08-21 01:37:50 -0700

    Extra language strings.

--- system/language/en-US.php
+++ system/language/en-US.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-	'ERROR_500'         => 'Internal Server Error',
-	'ERROR_500_MESSAGE' => 'The server has encountered an error and could not process your request.',
-	'EXCEPTION'         => 'Exception',
-	'TYPE'              => 'Type',
-	'MESSAGE'           => 'Message',
-	'FILE'              => 'File',
-	'TRACE'             => 'Trace',
-	'OUTPUT'            => 'Output'
+	'ERROR_500'                   => 'Internal Server Error',
+	'ERROR_500_MESSAGE'           => 'The server has encountered an error and could not process your request.',
+	'EXCEPTION'                   => 'Exception',
+	'TYPE'                        => 'Type',
+	'MESSAGE'                     => 'Message',
+	'FILE'                        => 'File',
+	'TRACE'                       => 'Trace',
+	'OUTPUT'                      => 'Output',
+	'OUTPUT_BUFFER_NOT_EMPTY'     => 'Unexpected output occurred.',
+	'OUTPUT_BUFFER_LEVEL_DIFFERS' => 'Unexpected output buffering level change.'
 ];
