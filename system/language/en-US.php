2f738a31427ac5f72ab26f1df181c187b461d158
Matthew Oltendorf <moltendorf@gmail.com>
2014-08-22 10:50:35 -0700

    Updated language files.

--- system/language/en-US.php
+++ system/language/en-US.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+	'STATUS_301'                  => 'Moved Permanently',
+	'STATUS_301_MESSAGE'          => 'You are being redirected to:',
+	'STATUS_302'                  => 'Moved',
+	'STATUS_302_MESSAGE'          => 'You are being redirected to:',
+	'ERROR_404'                   => 'Not Found',
+	'ERROR_404_MESSAGE'           => 'The page you requested was not found.',
 	'ERROR_500'                   => 'Internal Server Error',
 	'ERROR_500_MESSAGE'           => 'The server has encountered an error and could not process your request.',
 	'EXCEPTION'                   => 'Exception',
