4f8633aea09e0640129fc78f7883f7587de9ee42
Matthew Oltendorf <matthew@oltendorf.net>
2016-03-02 11:52:33 -0800

    Remove header and footer for print views.

--- style/default/template/footer.html.php
+++ style/default/template/footer.html.php
@@ -1,7 +1,11 @@
-<footer id="footer-wrap">
-	<?php if (isset($v['core_copyright'])): ?>
-		<div id="footer-copyright"><?= $l['COPYRIGHT'] ?> <?= $v['core_copyright_date'] ?> <?= $v['core_copyright_display'] ?></div>
-	<?php endif ?>
-	<div id="footer-powered-by"><?= $l['POWERED_BY'] ?></div>
-</footer>
+<?php if (empty($v['print'])): ?>
+	<footer id="footer-wrap">
+		<?php if (isset($v['core_copyright'])): ?>
+			<div id="footer-copyright">
+				<?= $l['COPYRIGHT'] ?> <?= $v['core_copyright_date'] ?> <?= $v['core_copyright_display'] ?>
+			</div>
+		<?php endif ?>
+		<div id="footer-powered-by"><?= $l['POWERED_BY'] ?></div>
+	</footer>
+<?php endif ?>
 <?php $f('footer_base.html') ?>
--- style/default/template/header.html.php
+++ style/default/template/header.html.php
@@ -1,15 +1,17 @@
 <?php $f('header_base.html') ?>
-<header id="site-header" class="fancy-box">
-	<h1><a id="site-title" href="<?= $v['core_root'] ?? '.' ?>"><?= $v['core_display'] ?? $_SERVER['HTTP_HOST'] ?></a>
-		<?php if (isset($v['core_tagline'])): ?>
-			<small id="site-tagline"><?= $v['core_tagline'] ?></small>
-		<?php endif ?></h1>
+<?php if (empty($v['print'])): ?>
+	<header id="site-header" class="fancy-box">
+		<h1><a id="site-title" href="<?= $v['core_root'] ?? '.' ?>"><?= $v['core_display'] ?? $_SERVER['HTTP_HOST'] ?></a>
+			<?php if (isset($v['core_tagline'])): ?>
+				<small id="site-tagline"><?= $v['core_tagline'] ?></small>
+			<?php endif ?></h1>
 
-	<?php if (isset($v['core_navigation'])): ?>
-		<nav id="site-nav">
-			<?php foreach ($v['core_navigation'] as $m): ?>
-				<a class="site-nav<?= $m['active'] ? ' active' : '' ?>" href="<?= $m['path'] ?>"><?= $m['display'] ?></a>
-			<?php endforeach ?>
-		</nav>
-	<?php endif ?>
-</header>
+		<?php if (isset($v['core_navigation'])): ?>
+			<nav id="site-nav">
+				<?php foreach ($v['core_navigation'] as $m): ?>
+					<a class="site-nav<?= $m['active'] ? ' active' : '' ?>" href="<?= $m['path'] ?>"><?= $m['display'] ?></a>
+				<?php endforeach ?>
+			</nav>
+		<?php endif ?>
+	</header>
+<?php endif ?>
